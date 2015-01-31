e341cfb1d73ae1d388d0226e1616410ddb98cc93
yamamoto <yamamoto@kyms.jp>
2015-02-01 00:29:28 +0900

    mox
    http://forum.nucleuscms.org/viewtopic.php?p=101639#101639

--- NP_MemberOnly.php
+++ NP_MemberOnly.php
@@ -15,14 +15,19 @@ function getDescription() { return 'MemberOnly'; }
 
 	function event_PreItem($data){
 		global $member;
-		if ($member && $member->isLoggedIn())
-		{
-			return;
-		}
 		
 		$this->currentItem = &$data["item"]; 
-		$this->currentItem->more = preg_replace("#(</mo>)<br />\r\n#","$1",$this->currentItem->more);
-		$this->currentItem->body = preg_replace('#<mo>(.*?)</mo>#s', '', $this->currentItem->body); 
-		$this->currentItem->more = preg_replace('#<mo>(.*?)</mo>#s', '', $this->currentItem->more);
+		if ($member && $member->isLoggedIn())
+		{
+			$this->currentItem->more = preg_replace("#(</mox>)<br />\r\n#","$1",$this->currentItem->more);
+			$this->currentItem->body = preg_replace('#<mox>(.*?)</mox>#s', "$1", $this->currentItem->body); 
+			$this->currentItem->more = preg_replace('#<mox>(.*?)</mox>#s', "$1", $this->currentItem->more);
+		}
+		else
+		{
+			$this->currentItem->more = preg_replace("#(</mo>)<br />\r\n#","$1",$this->currentItem->more);
+			$this->currentItem->body = preg_replace('#<mo>(.*?)</mo>#s', '', $this->currentItem->body); 
+			$this->currentItem->more = preg_replace('#<mo>(.*?)</mo>#s', '', $this->currentItem->more);
+		}
 	}
 }
