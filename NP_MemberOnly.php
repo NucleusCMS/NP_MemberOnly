cdabe216739c7bf25ba1596e1c4de00acf45860a
yamamoto <yamamoto@kyms.jp>
2015-12-27 20:02:42 +0900

    Refactor

--- NP_MemberOnly.php
+++ NP_MemberOnly.php
@@ -2,38 +2,35 @@
 
 class NP_MemberOnly extends NucleusPlugin {
 
-function getName() { return 'MemberOnly';    }
-function getAuthor() { return 'kimitake';    }
-function getURL() { return 'http://kimitake.blogdns.net'; }
-function getVersion() { return '0.1'; }
-function getDescription() { return 'MemberOnly'; }
+    function getName()        { return 'MemberOnly';}
+    function getAuthor()      { return 'kimitake';}
+    function getURL()         { return 'https://github.com/NucleusCMS/NP_MemberOnly'; }
+    function getVersion()     { return '0.2'; }
+    function getDescription() { return 'MemberOnly'; }
 
-	function install() {
-	}
+    function getEventList() { return array('PreItem'); }
 
-	function getEventList() { return array('PreItem'); }
-
-	function event_PreItem($data){
-		global $member;
-		
-		$item = &$data['item']; 
-		if ($member && $member->isLoggedIn())
-		{
-			$item->more = preg_replace("#(</mo>)<br />\r\n#","$1", $item->more);
-			$item->body = preg_replace('#<mo>(.*?)</mo>#s',  "$1", $item->body); 
-			$item->more = preg_replace('#<mo>(.*?)</mo>#s',  "$1", $item->more);
-			$item->more = preg_replace("#(</mox>)<br />\r\n#",'',  $item->more);
-			$item->body = preg_replace('#<mox>(.*?)</mox>#s', '',  $item->body); 
-			$item->more = preg_replace('#<mox>(.*?)</mox>#s', '',  $item->more);
-		}
-		else
-		{
-			$item->more = preg_replace("#(</mox>)<br />\r\n#","$1", $item->more);
-			$item->body = preg_replace('#<mox>(.*?)</mox>#s', "$1", $item->body); 
-			$item->more = preg_replace('#<mox>(.*?)</mox>#s', "$1", $item->more);
-			$item->more = preg_replace("#(</mo>)<br />\r\n#","$1",  $item->more);
-			$item->body = preg_replace('#<mo>(.*?)</mo>#s', '',     $item->body); 
-			$item->more = preg_replace('#<mo>(.*?)</mo>#s', '',     $item->more);
-		}
-	}
+    function event_PreItem($data){
+        global $member;
+        
+        $item = &$data['item']; 
+        if ($member && $member->isLoggedIn())
+        {
+            $item->more = preg_replace("#(</mo>)<br />\r\n#","$1", $item->more);
+            $item->body = preg_replace('#<mo>(.*?)</mo>#s',  "$1", $item->body); 
+            $item->more = preg_replace('#<mo>(.*?)</mo>#s',  "$1", $item->more);
+            $item->more = preg_replace("#(</mox>)<br />\r\n#",'',  $item->more);
+            $item->body = preg_replace('#<mox>(.*?)</mox>#s', '',  $item->body); 
+            $item->more = preg_replace('#<mox>(.*?)</mox>#s', '',  $item->more);
+        }
+        else
+        {
+            $item->more = preg_replace("#(</mox>)<br />\r\n#","$1", $item->more);
+            $item->body = preg_replace('#<mox>(.*?)</mox>#s', "$1", $item->body); 
+            $item->more = preg_replace('#<mox>(.*?)</mox>#s', "$1", $item->more);
+            $item->more = preg_replace("#(</mo>)<br />\r\n#","$1",  $item->more);
+            $item->body = preg_replace('#<mo>(.*?)</mo>#s', '',     $item->body); 
+            $item->more = preg_replace('#<mo>(.*?)</mo>#s', '',     $item->more);
+        }
+    }
 }
